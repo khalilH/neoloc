51221ea738b1339b40e007bb32bc45557acf5279
BDevSEPLPPOL <pp-dostl-sdsicif-sepl-developpement@interieur.gouv.fr>
2017-02-09 12:53:54 +0100

    config.js.vide

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 var NEOCONFIG = {
 		es:{
-			host:"http://plf.poc.plf-sso.ppol.minint.fr/es",
-			index:"neo",
+			host:"",
+			index:"",
 			type:{
-				neo:"tests",
-				event:"event"
+				neo:"",
+				event:""
 			}
 		},
 		mapServer:{
-			template:"http://plf.poc.plf-sso.ppol.minint.fr/esri/server/rest/services/PP/FOND_PP_FINAL_512/MapServer/tile/{z}/{y}/{x}"			
+			template:""			
 		}
 };
